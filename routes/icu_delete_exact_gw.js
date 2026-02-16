14222d843c9ae1e5bd4915edd9cc45dfef362360
root <root@server>
2026-02-16 12:32:09 +0000

    fix: restore DELETE /gw/icu/events route (attach window-delete shim)

--- routes/icu_delete_exact_gw.js
+++ routes/icu_delete_exact_gw.js
@@ -1,3 +1,7 @@
-module.exports = (app) => {
-  console.log('[v2][noop] icu_delete_exact_gw disabled (stub)');
+// Restored: DELETE /gw/icu/events window-delete shim
+// server.js expects: require("./routes/icu_delete_exact_gw")(app)
+
+module.exports = function(app){
+  require("../lib/attach_delete")(app);
+  console.log("[icu][DELETE] /gw/icu/events attached (window shim via lib/attach_delete.js)");
 };
